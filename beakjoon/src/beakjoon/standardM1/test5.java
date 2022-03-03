64734034d38478ea4278aff17157ab3bbf84ef91
parkq5231 <parkq5231>
2022-03-03 23:04:39 +0900

    2020303

---
+++ beakjoon/src/beakjoon/standardM1/test5.java
@@ -0,0 +1,29 @@
+package beakjoon.standardM1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int H = Integer.parseInt(st.nextToken());
+			st.nextToken();// W
+			int N = Integer.parseInt(st.nextToken());
+
+			if (N % H == 0) {
+				sb.append((H * 100) + (N / H)).append("\n");
+			} else {
+				sb.append((N % H) * 100 + ((N / H) + 1)).append("\n");
+			}
+		} // end of for
+		System.out.println(sb);
+	}// end of main
+}// end of class
---
+++ beakjoon/src/beakjoon/standardM1/test6.java
@@ -0,0 +1,37 @@
+package beakjoon.standardM1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test6 {
+	public static int[][] APT = new int[15][15];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		make_APT();// 아파트 생성
+
+		int T = Integer.parseInt(br.readLine());// test_case
+
+		for (int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			sb.append(APT[k][n]).append("\n");
+		} // end of for
+		System.out.println(sb);
+	}// end of main
+
+	public static void make_APT() {
+		for (int i = 0; i < 15; i++) {
+			APT[i][1] = 1;
+			APT[0][i] = i;
+		}
+		for (int i = 1; i < 15; i++) {// 1~14층
+			for (int j = 2; j < 15; j++) {
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+	}// end of make_APT
+}// end of class
